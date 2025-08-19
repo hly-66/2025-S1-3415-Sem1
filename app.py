1f7fe1b6ee40a171698f35f73a6afc63c9471b05
hly-66 <hannahweiaily@gmail.com>
2025-08-19 03:43:24 +0000

    1

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-print('starting flask')
-
 from flask import Flask,render_template,request
 
 app = Flask(__name__)   #confirm the name of the registered person who is running the application
@@ -9,6 +7,16 @@ app = Flask(__name__)   #confirm the name of the registered person who is runnin
 @app.route("/",methods=["GET","POST"]) #using flask framework, this is the way we are coding; "/" is the landing page for flask; methods is what you need to run before you can run anything else
 def index():
     return(render_template("index.html"))
+
+@app.route("/main",methods=["GET","POST"])
+def main():
+    #database
+    return(render_template("main.html"))
+
+@app.route("/dbs",methods=["GET","POST"])
+def dbs():
+    q = float(request.form.get("q"))
+    return(render_template("dbs.html", r= (-50.6*q)+90.2))
 
 if __name__ == "__main__":
     app.run()   #confirming the registered person wants to run the program
