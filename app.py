feb8d5963304994187951f6138c35e14b7ee8a02
hly-66 <hannahweiaily@gmail.com>
2025-08-19 02:48:18 +0000

    1

---
+++ app.py
@@ -0,0 +1,14 @@
+print('starting flask')
+
+from flask import Flask,render_template,request
+
+app = Flask(__name__)   #confirm the name of the registered person who is running the application
+
+#gunicorn.py is a program to run wasgi and wasgi is needed for python to communicate frontend and backend
+
+@app.route("/",methods=["GET","POST"]) #using flask framework, this is the way we are coding; "/" is the landing page for flask; methods is what you need to run before you can run anything else
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()   #confirming the registered person wants to run the program
